f45612ed8397cd9189a4994359c674f9f20f7adf
Michael Villis <mvillis@gmail.com>
2015-12-14 18:06:17 +1000

    update wsgi to remove duplicate line

--- measure_mate/wsgi.py
+++ measure_mate/wsgi.py
@@ -6,12 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "measure_mate.settings")
+
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
