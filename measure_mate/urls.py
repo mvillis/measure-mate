e908c3c939bfa8fe93099b89b6888ba6a1cb3bcf
Michael Villis <mvillis@gmail.com>
2015-12-28 21:36:50 +1000

    redirect index to assessment obj view plus view tests

--- measure_mate/urls.py
+++ measure_mate/urls.py
@@ -13,8 +13,8 @@ router.register(r'assessments', AssessmentViewSet)
 router.register(r'ratings', RatingViewSet)
 
 urlpatterns = [
-    url(r'^assessment/list$', assessmentList, name='assessmentlist'),
-    url(r'^assessment/$', assessment, name='assessment'),
+    url(r'^assessment/list$', assessment_list, name='assessmentlist'),
+    url(r'^assessment/(?P<pk>[0-9]+)/$', assessment, name='assessment'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/tags?', TagListView.as_view()),
     url(r'^api/', include(router.urls)),
