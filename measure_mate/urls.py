56d3afb1331381b6c47e775783c9705b24b71897
Michael Villis <mvillis@gmail.com>
2016-01-13 08:10:17 +1000

    promoted beta view to primary

--- measure_mate/urls.py
+++ measure_mate/urls.py
@@ -14,11 +14,9 @@ router.register(r'ratings', RatingViewSet)
 
 urlpatterns = [
     url(r'^assessment/list$', assessment_list, name='assessmentlist'),
-    url(r'^assessment-revised/(?P<pk>[0-9]+)/$', assessment_revised, name='assessment-revised'),
     url(r'^assessment/(?P<pk>[0-9]+)/$', assessment, name='assessment'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/tags?', TagListView.as_view()),
-    # url(r'^api/measurements/?', MeasurementListView.as_view()),
     url(r'^api/', include(router.urls)),
     url(r'^api/api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
