d46740682e543bbf28791ca242d8e4086431c003
Robert Loomans <robert@loomans.org>
2016-01-20 15:27:59 +1000

    customise the Swagger output

--- measure_mate/settings.py
+++ measure_mate/settings.py
@@ -144,3 +144,17 @@ LOGGING = {
 }
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+SWAGGER_SETTINGS = {
+    'api_path': '/api',
+    'api_version': '0.1',
+    'info': {
+        'title': 'Measure Mate',
+        'description': '<p>Simple tool to track maturity assessments.</p>'
+                       '<p>Source available at <a href="https://github.com/mvillis/measure-mate">'
+                       'https://github.com/mvillis/measure-mate</a>.</p>',
+        'license': 'MIT',
+        'licenseUrl': 'https://github.com/mvillis/measure-mate/blob/master/LICENSE.txt',
+    }
+}
+
