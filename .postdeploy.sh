00e1fdbb5136324f30c39f9fd70cef65fc381f8f
Robert Loomans <robert@loomans.org>
2016-02-03 15:39:35 +1000

    * Add an assessment fixture * Try to load the assessment fixture during the post deploy

--- .postdeploy.sh
+++ .postdeploy.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
 python manage.py migrate
-python manage.py loaddata LAMA DevOps tags
+python manage.py loaddata LAMA Governance DevOps tags
+
+# won't work if there are already assessments
+python manage.py loaddata assessment || true
