89a3c08b7a211b2dc233d9e643a6f8a9001d4141
Robert Loomans <robert@loomans.org>
2016-02-25 00:02:47 +1000

    naming consistency

--- measure_mate/urls.py
+++ measure_mate/urls.py
@@ -14,7 +14,7 @@ router.register(r'ratings', RatingViewSet)
 router.register(r'teams', TeamViewSet)
 
 urlpatterns = [
-    url(r'^assessment/list$', assessment_list, name='assessmentlist'),
+    url(r'^assessment/list$', assessment_list, name='assessment-list'),
     url(r'^assessment/(?P<pk>[0-9]+)/$', assessment, name='assessment'),
     url(r'^assessment/report/(?P<pk>[0-9]+)/$', assessment_report, name='assessment-report'),
     url(r'^admin/', include(admin.site.urls)),
