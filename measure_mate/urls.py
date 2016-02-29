6f421285922a30b14f5a7142e9e1317e70ca40a6
Robert Loomans <robert@loomans.org>
2016-03-01 09:09:08 +1000

    drop unneeded code

--- measure_mate/urls.py
+++ measure_mate/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     url(r'^assessment/(?P<pk>[0-9]+)/$', assessment, name='assessment'),
     url(r'^assessment/report/(?P<pk>[0-9]+)/$', assessment_report, name='assessment-report'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/tags?', TagListView.as_view()),
     url(r'^api/', include(router.urls)),
     url(r'^api/api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
