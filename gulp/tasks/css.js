619a35842c2be880246ed8b6084803f294586b5a
Robert Loomans <robert@loomans.org>
2016-03-14 23:51:27 +1000

    add sourcemap for bundle.css

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var sourcemaps = require('gulp-sourcemaps')
 var browserSync = require('browser-sync')
 var handleErrors = require('../util/handleErrors')
 var config = require('../config').css
 
 gulp.task('css', function () {
   gulp.src(config.src)
+    .pipe(sourcemaps.init())
     .pipe(concat('bundle.css'))
     .on('error', handleErrors)
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream: true}))
 })
