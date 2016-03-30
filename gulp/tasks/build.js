97870d7ade62fff0da098bff5a2e3fba0b352e45
Robert Loomans <robert@loomans.org>
2016-03-30 16:21:31 +1000

    whitenoise takes care of gzipping

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,6 +2,6 @@ var gulp = require('gulp')
 var runSequence = require('run-sequence')
 
 gulp.task('build', function (callback) {
-  runSequence('envSetup', ['browserify', 'css', 'fonts', 'assets', 'templates', 'lint', 'lintCss', 'markdownlint'], 'gzip', callback)
+  runSequence('envSetup', ['browserify', 'css', 'fonts', 'assets', 'templates', 'lint', 'lintCss', 'markdownlint'], callback)
   global.isBuilding = false
 })
