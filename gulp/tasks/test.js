42ed67c78b2a204d31b9c04b0c9eedac7456f561
Michael Villis <mvillis@gmail.com>
2016-04-25 22:43:58 +1000

    updated test step to produce coverage info

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,9 +1,26 @@
 var gulp = require('gulp')
 var mocha = require('gulp-mocha')
+var babel = require('gulp-babel')
+var istanbul = require('gulp-babel-istanbul')
+var injectModules = require('gulp-inject-modules')
 var config = require('../config').test
 require('babel-core/register')
 
-gulp.task('test', function () {
-  return gulp.src(config.src, {read: false})
-      .pipe(mocha(config.mochaOptions))
+gulp.task('pre-test', function () {
+  return gulp.src(config.src)
+    // Covering files
+    .pipe(istanbul({includeUntested: true}))
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire())
+})
+
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(config.testSrc)
+    .pipe(babel())
+		.pipe(injectModules())
+    .pipe(mocha(config.mochaOptions))
+    // Creating the reports after tests ran
+    .pipe(istanbul.writeReports(config.istanbulReportOptions))
+    // Enforce a coverage of at least 90%
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
 })
