d103fdae6022e8f8eb1acbdf014af5aa93633016
Robert Loomans <robert@loomans.org>
2016-05-12 10:59:54 +1000

    simplify home page test

--- client/test/js/components/home/home.test.js
+++ client/test/js/components/home/home.test.js
@@ -3,22 +3,13 @@
 var React = require('react')
 var expect = require('chai').expect
 var enzyme = require('enzyme')
-var shallow = enzyme.shallow
 var render = enzyme.render
 var Home = require('../../../../js/components/home/home')
 
 describe('Home Page Component', function () {
-  it('contains slogan text class', function () {
-    const wrapper = shallow(<Home/>)
-    expect(wrapper
-    .find('.logo-slogan-text'))
-    .to
-    .have.length(1)
-  })
-
   it('contains slogan text', function () {
     const wrapper = render(<Home/>)
-    expect(wrapper.text())
+    expect(wrapper.find('.logo-slogan-text').text())
     .to
     .contain('A tool to track maturity assessments for your team')
   })
