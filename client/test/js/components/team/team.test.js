366ca91fb0bb7e86cbb9fdaf94e0f93fbf6e5863
Robert Loomans <robert@loomans.org>
2016-05-12 14:53:57 +1000

    better description of the test

--- client/test/js/components/team/team.test.js
+++ client/test/js/components/team/team.test.js
@@ -10,7 +10,7 @@ var Team = require('../../../../js/components/team/team')
 var mocked_params = {'teamId': 1}
 
 describe('Team Page Component', function () {
-  it('contains Panels', function () {
+  it('contains team details', function () {
     const wrapper = shallow(<Team params={mocked_params} />)
 
     expect(wrapper.find(Panel))
