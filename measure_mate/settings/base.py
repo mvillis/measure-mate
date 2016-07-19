370427f3f5d0f61789c8e2f9a72ffdb046dfb7fb
Robert Loomans <robert@loomans.org>
2016-07-19 18:29:00 +1000

    allow fonts from google

--- measure_mate/settings/base.py
+++ measure_mate/settings/base.py
@@ -215,7 +215,7 @@ CSP_SCRIPT_SRC = ("'unsafe-eval'", "'unsafe-inline'", "'self'",)
 CSP_CONNECT_SRC = ("'self'",)
 CSP_STYLE_SRC = ("'unsafe-inline'", "'self'", "fonts.googleapis.com",)
 CSP_IMG_SRC = ("'self'", "data:", "blob:",)
-CSP_FONT_SRC = ("'self'", "fonts.googleapis.com",)
+CSP_FONT_SRC = ("'self'", "fonts.gstatic.com",)
 CSP_EXCLUDE_URL_PREFIXES = ("/admin",)
 
 LOGIN_URL = "/admin/login/"
