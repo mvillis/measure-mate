5ddf98f6a6e586aa2d01d23b3280e37c2bbb5bd0
Robert Loomans <robert@loomans.org>
2016-07-19 14:57:24 +1000

    kill extraneous div and extraneous definition of key prop

--- client/js/components/assessment/rating.js
+++ client/js/components/assessment/rating.js
@@ -11,7 +11,6 @@ var Popover = ReactBootstrap.Popover
 
 var Rating = React.createClass({
   propTypes: {
-    key: React.PropTypes.number,
     eventKey: React.PropTypes.number,
     measurement: React.PropTypes.object,
     assessId: React.PropTypes.string.isRequired,
