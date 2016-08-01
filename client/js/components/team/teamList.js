3d43a305d244b8d6cea84bb1884666d0483f017d
Robert Loomans <robert@loomans.org>
2016-08-01 16:48:30 +1000

    allow sorting assessment and team lists

--- client/js/components/team/teamList.js
+++ client/js/components/team/teamList.js
@@ -1,27 +1,33 @@
 'use strict'
 
 var React = require('react')
+var ReactDOM = require('react-dom')
 var ReactBootstrap = require('react-bootstrap')
 var ReactRouterBootstrap = require('react-router-bootstrap')
 var LinkContainer = ReactRouterBootstrap.LinkContainer
 var Moment = require('moment')
 var Table = ReactBootstrap.Table
 var TagList = require('../common/tagList')
+var tablesort = require('tablesort')
 
 var TeamList = React.createClass({
   propTypes: {
     teams: React.PropTypes.array.isRequired,
     teamTags: React.PropTypes.array
   },
+  componentDidMount: function () {
+    var teamList = ReactDOM.findDOMNode(this.refs.teamList)
+    tablesort(teamList)
+  },
 
   render: function () {
     return (
-      <Table hover>
+      <Table ref='teamList' hover>
         <thead>
           <tr>
             <th>#</th>
             <th>Team Name</th>
-            <th>Tags</th>
+            <th className='no-sort'>Tags</th>
             <th>Created Date</th>
           </tr>
         </thead>
@@ -34,12 +40,14 @@ var TeamList = React.createClass({
             return (
               <LinkContainer key={team.id} to={{pathname: teamUrl}}>
                 <tr className='clickable'>
-                  <td>
+                  <td data-sort={team.id} data-sort-method='number'>
                     <a href={teamUrl}>{team.id}</a>
                   </td>
                   <td>{team.name}</td>
                   <td className='wrap'><TagList tags={tags} /></td>
-                  <td>{prettyCreated} <small>({relativeCreated})</small></td>
+                  <td data-sort={team.created} data-sort-method='date'>
+                    {prettyCreated} <small>({relativeCreated})</small>
+                  </td>
                 </tr>
               </LinkContainer>
             )
