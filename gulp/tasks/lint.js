f30e73cb8ae7fb087bda04a7147fbb9af6bc6a91
Robert Loomans <robert@loomans.org>
2016-09-02 09:45:12 +1000

    lint errors

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -3,14 +3,14 @@ var util = require('gulp-util')
 var semver = require('semver')
 
 if (semver.gt(process.version, '1.0.0')) {
-    var lint = require('gulp-eslint')
-    var config = require('../config').lint
+  var lint = require('gulp-eslint')
+  var config = require('../config').lint
 
-    gulp.task('lint', function () {
-      return gulp.src(config.src)
+  gulp.task('lint', function () {
+    return gulp.src(config.src)
         .pipe(lint({config: config.configSrc}))
         .pipe(lint.format())
-    })
+  })
 } else {
   gulp.task('lint', function () {
     util.log(util.colors.magenta('WARNING:'), ' \'lint\' task skipped: NodeJS is too old.')
