50735729348b89c3728703e7e30687cf93e3a36d
Robert Loomans <robert@loomans.org>
2016-12-19 13:12:06 +1000

    dont load markdownlint for production

--- gulp/tasks/lintMarkdown.js
+++ gulp/tasks/lintMarkdown.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp')
 var util = require('gulp-util')
 var through2 = require('through2')
-var markdownlint = require('markdownlint')
 var config = require('../config').markdownlint
 
 if (config.production) {
@@ -9,6 +8,8 @@ if (config.production) {
     util.log(util.colors.magenta('WARNING:'), ' \'lint-markdown\' task skipped in production.')
   })
 } else {
+  var markdownlint = require('markdownlint')
+
   gulp.task('lint-markdown', function () {
     return gulp.src(config.src, { 'read': false })
       .pipe(through2.obj(function (file, enc, next) {
