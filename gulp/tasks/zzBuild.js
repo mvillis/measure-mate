4de6d4855c6c213711fb85bb586ea7a80b4d0bd7
Robert Loomans <robert@loomans.org>
2017-02-17 16:53:06 +1000

    kill a complaint from gulp

--- gulp/tasks/zzBuild.js
+++ gulp/tasks/zzBuild.js
@@ -7,7 +7,7 @@ gulp.task('build', gulp.series(
     'build:js', 'build:css', 'build:fonts', 'assets'
   ),
   gulp.parallel(
-    config.production ? [] : ['lint:js', 'lint:css', 'lint:markdown', 'test']
+    'lint:js', 'lint:css', 'lint:markdown', 'test'
   ),
   function buildDone (done) {
     global.isBuilding = false
