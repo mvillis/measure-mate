6332da11b3931b63ab43da14b0fca293b5fcaa19
Robert Loomans <robert@loomans.org>
2017-02-17 13:48:04 +1000

    try to avoid loading browser-sync

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,7 +7,7 @@ var gulp = require('gulp')
 var util = require('gulp-util')
 var config = require('../config')
 
-if (config.production) {
+if (config.production || util.env.production) {
   gulp.task('watch', function (done) {
     util.log(util.colors.magenta('WARNING:'), ' \'watch\' task skipped in production.')
     done()
